31f686e399d5c1ee812aa8465d8b408202798a6b
Nazar Baranovskyi <baranovskiy_25@mail.ru>
2019-03-01 23:40:49 +0200

    1.3.0 New info

--- assets/main.js
+++ assets/main.js
@@ -1,20 +1,45 @@
 $(document).ready(function() {
+//  ----- AUTO SIZE OF HOW WORK HEAD BLOCKS -----
+	let howWorkHeadBlock        = document.getElementsByClassName("how-work__head");
+		howWorkHeadBlockSizes   = [],
+		howWorkHeadBlockMaxSize = howWorkHeadBlock[0].offsetHeight;
+
+	for (let i = 0; i<howWorkHeadBlock.length; i++) {
+		howWorkHeadBlockSizes[i] = howWorkHeadBlock[i].offsetHeight;
+
+		if(howWorkHeadBlockSizes[i] > howWorkHeadBlockMaxSize) {
+			howWorkHeadBlockMaxSize = howWorkHeadBlockSizes[i];
+		}
+	};
+	
+	for (let i = 0; i<howWorkHeadBlock.length; i++) {
+		howWorkHeadBlock[i].style.paddingTop    = (howWorkHeadBlockMaxSize-howWorkHeadBlockSizes[i])/2+'px';
+		howWorkHeadBlock[i].style.paddingBottom = (howWorkHeadBlockMaxSize-howWorkHeadBlockSizes[i])/2+'px';
+	};
+
 //  ----- AUTO SIZE OF HOW WORK BLOCKS -----
 	let howWorkBlock = document.getElementsByClassName("how-work__description"),
-		blockSizes = [],
-		maxSize = howWorkBlock[0].offsetHeight;
+		blockSizes   = [],
+		maxSize      = howWorkBlock[0].offsetHeight;
 
 	if(document.documentElement.clientWidth>991) {
 		for (let i = 0; i<howWorkBlock.length; i++) {
 			blockSizes[i] = howWorkBlock[i].offsetHeight;
 			if(blockSizes[i]>maxSize) {
 				maxSize = blockSizes[i];
-			}
-		}
+			};
+		};
 		for (let i = 0; i<howWorkBlock.length; i++) {
 			howWorkBlock[i].style.paddingBottom = maxSize-blockSizes[i]+5+'px';
-		}
+		};
 	};
+
+//  ----- HEIGHT OF PROJECTS CONTAINER -----
+	
+	let heightOfProjectItem      = document.getElementsByClassName("projects__item")[0].offsetHeight,
+		heightOfProjectContainer = document.getElementsByClassName("projects__container")[0];
+
+	heightOfProjectContainer.style.height = heightOfProjectItem + 6 + 'px';
 
 //  ----- SHOW/HIDE PROJECTS ------
 	let getAll     = document.getElementById("btn-all"),
@@ -25,17 +50,15 @@ $(document).ready(function() {
 
 	let typeAll = document.getElementsByClassName("projects__item");
 
-	getAll.onclick = function() {
-		for (let i=0; i<typeAll.length; i++) {
-			typeAll[i].classList.remove('show');
-			typeAll[i].classList.remove('hide');
-
-			typeAll[i].classList.add('show');
-		};
-	};
+	let projectsButtons = document.getElementsByClassName("projects__btn");
 
 	function changesClasses(button, elem) {
 		button.onclick = function() {
+			for (let i=0; i<projectsButtons.length; i++) {
+				projectsButtons[i].classList.remove('projects__btn--active');
+			}
+			this.classList.add('projects__btn--active');
+
 			for (let i=0; i<typeAll.length; i++) {
 				typeAll[i].classList.remove('show');
 				typeAll[i].classList.remove('hide');
@@ -48,7 +71,7 @@ $(document).ready(function() {
 			};
 		};
 	};
-
+	changesClasses(getAll, 'projects__item');
 	changesClasses(getLogo, 'logo');
 	changesClasses(getLanding, 'landing');
 	changesClasses(getWebsite, 'website');
@@ -66,5 +89,4 @@ $(document).ready(function() {
 			upToTopBlock[0].style.bottom = '-60px';
 		};
 	};
-
 });
